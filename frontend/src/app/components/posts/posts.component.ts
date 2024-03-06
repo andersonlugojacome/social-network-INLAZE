4af018f59f7e0850bfff44fe409390ed46ff78a1
Anderson Lugo <andersonlugojacome@hotmail.com>
2024-03-06 10:56:28 -0500

    Update navbar component and posts component

--- frontend/src/app/components/posts/posts.component.ts
+++ frontend/src/app/components/posts/posts.component.ts
@@ -14,85 +14,103 @@ export class PostsComponent implements OnInit {
 
   ngOnInit(): void {
     this.getPosts();
-   
+
   }
   getPosts() {
     this.postsService.getPosts().subscribe(
       res => {
         this.posts = res;
-        },
+      },
       err => console.log(err)
     );
-  }
-  // onEdit(post: Posts) {
-  //   this.postsService.selectedPost = post;
-  // }
-  onDelete(id: string) {
+    // var myArray = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3];
+    // //Creo un objeto para almacenar las frecuencias de cada número
+    // const histograma: { [key: number]: string } = {};
 
-    //agregamos un alert para confirmar la eliminacion del post
-    if (confirm('Are you sure you want to delete it?')) {
-      this.postsService.deletePost(id).subscribe(
-        res => {
-          console.log(res);
-          this.getPosts();
-        },
-        err => console.log(err)
-      );
-    }else
-    {
-      this.getPosts();
+    // // Inicializo el histograma con todas las frecuencias en cero
+    // for (let i = 1; i <= 5; i++) {
+    //   histograma[i] = '';
+    // }
+    // // Recorro el arreglo y cuento cual se repite de cada número
+    // myArray.forEach(numero => {
+    //   histograma[numero] += '*';
+    // });
+
+    // // Muestro el histograma
+    // for (let i = 1; i <= 5; i++) {
+    //   console.log(`${i}: ${histograma[i]}`);
+    // }
+  }
+    // onEdit(post: Posts) {
+    //   this.postsService.selectedPost = post;
+    // }
+    onDelete(id: string) {
+
+      //agregamos un alert para confirmar la eliminacion del post
+      if (confirm('Are you sure you want to delete it?')) {
+        this.postsService.deletePost(id).subscribe(
+          res => {
+            console.log(res);
+            this.getPosts();
+          },
+          err => console.log(err)
+        );
+      } else {
+        this.getPosts();
+      }
+
+
+      // this.postsService.deletePost(id).subscribe(
+      //   res => {
+      //     console.log(res);
+      //     this.getPosts();
+      //   },
+      //   err => console.log(err)
+      // );
+    }
+
+    onLike(id: string) {
+      // this.postsService.likePost(id).subscribe(
+      //   res => {
+      //     console.log(res);
+      //     this.getPosts();
+      //   },
+      //   err => console.log(err)
+      // );
+    }
+    onDislike(id: string) {
+      // this.postsService.dislikePost(id).subscribe(
+      //   res => {
+      //     console.log(res);
+      //     this.getPosts();
+      //   },
+      //   err => console.log(err)
+      // );
     }
 
 
-    // this.postsService.deletePost(id).subscribe(
-    //   res => {
-    //     console.log(res);
-    //     this.getPosts();
-    //   },
-    //   err => console.log(err)
-    // );
+
+
+    // likePost(id: string) {
+    //   this.postsService.likePost(id).subscribe(
+    //     res => {
+    //       console.log(res);
+    //       this.getPosts();
+    //     },
+    //     err => console.log(err)
+    //   );
+    // }
+    // dislikePost(id: string) {
+    //   this.postsService.dislikePost(id).subscribe(
+    //     res => {
+    //       console.log(res);
+    //       this.getPosts();
+    //     },
+    //     err => console.log(err)
+    //   );
+    // }
+
+
+
+
   }
-
-  onLike(id: string) {
-    // this.postsService.likePost(id).subscribe(
-    //   res => {
-    //     console.log(res);
-    //     this.getPosts();
-    //   },
-    //   err => console.log(err)
-    // );
-  }
-  onDislike(id: string) {
-    // this.postsService.dislikePost(id).subscribe(
-    //   res => {
-    //     console.log(res);
-    //     this.getPosts();
-    //   },
-    //   err => console.log(err)
-    // );
-  }
-
-
-
-
-  // likePost(id: string) {
-  //   this.postsService.likePost(id).subscribe(
-  //     res => {
-  //       console.log(res);
-  //       this.getPosts();
-  //     },
-  //     err => console.log(err)
-  //   );
-  // }
-  // dislikePost(id: string) {
-  //   this.postsService.dislikePost(id).subscribe(
-  //     res => {
-  //       console.log(res);
-  //       this.getPosts();
-  //     },
-  //     err => console.log(err)
-  //   );
-  // }
-
-
-}
